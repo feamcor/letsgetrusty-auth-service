899df0fd14b7cd753cf97a02c2d9dd7a41e8baeb
Fábio Corrêa <feamcor@gmail.com>
2026-01-27 13:45:46 -0300

    [sprint 4] feat(auth-service): integrate `clap` for CLI argument parsing
    Added `clap` with `derive` and `env` features for command-line argument handling. Updated `main.rs` to parse configurations via CLI, including logging level, IP address, and port.

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -1,38 +1,52 @@
+mod config;
+
+use crate::config::Config;
 use auth_service::app_state::AppState;
 use auth_service::services::HashmapUserStore;
 use auth_service::Application;
-use std::net::SocketAddr;
-use std::sync::Arc;
+use clap::Parser;
 use dotenvy::dotenv_override;
 use fmt::format::FmtSpan;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::sync::Arc;
 use tokio::sync::RwLock;
-use tracing::{enabled, info, trace, Level};
+use tracing::info;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{fmt, EnvFilter};
 
 #[tokio::main]
 async fn main() {
-    let dotenv_loaded = dotenv_override().ok();
+    let dotenv = dotenv_override().ok();
+    let config = Config::parse();
+
     tracing_subscriber::registry()
-        .with(EnvFilter::from_default_env())
+        .with(EnvFilter::from(config.log.to_string()))
         .with(fmt::layer().with_span_events(FmtSpan::NEW | FmtSpan::CLOSE))
         .init();
     info!("Initialized: Tracing");
-    if dotenv_loaded.is_some() {
-        info!("Loaded file: {}", dotenv_loaded.unwrap().display());
+
+    if let Some(dotenv) = dotenv {
+        info!("Initialized: {}", dotenv.display());
     }
-    if enabled!(Level::TRACE) {
-        for var in dotenvy::vars() {
-            trace!("Loaded envar: {}={}", var.0, var.1);
-        }
-    }
+    info!("Initialized: {}", config);
+
     let user_store = HashmapUserStore::default();
     info!("Initialized: User store");
+
     let app_state = AppState::new(Arc::new(RwLock::new(user_store)));
     info!("Initialized: App state");
-    let socket_addr = SocketAddr::from(([0, 0, 0, 0], 3000));
+
+    let ip_address = if let Some(v6) = config.ipv6 {
+        IpAddr::V6(v6)
+    } else if let Some(v4) = config.ipv4 {
+        IpAddr::V4(v4)
+    } else {
+        IpAddr::V4(Ipv4Addr::UNSPECIFIED)
+    };
+    let socket_addr = SocketAddr::new(ip_address, config.port);
     info!("Initialized: Listening address: {}", socket_addr);
+
     Application::build(app_state, socket_addr)
         .await
         .expect("Failed to build app")
