7731c738ce8d281c7f094208536153a5bd7f9958
Fábio Corrêa <feamcor@gmail.com>
2026-01-27 11:22:33 -0300

    [sprint 3] feat(auth-service): add dotenvy for environment variable management
    Integrated `dotenvy` to enhance environment variable handling. Updated `main.rs` to load `.env` files and log loaded variables in trace mode.

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -3,20 +3,30 @@ use auth_service::services::HashmapUserStore;
 use auth_service::Application;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use dotenvy::dotenv_override;
 use fmt::format::FmtSpan;
 use tokio::sync::RwLock;
-use tracing::info;
+use tracing::{enabled, info, trace, Level};
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{fmt, EnvFilter};
 
 #[tokio::main]
 async fn main() {
+    let dotenv_loaded = dotenv_override().ok();
     tracing_subscriber::registry()
         .with(EnvFilter::from_default_env())
         .with(fmt::layer().with_span_events(FmtSpan::NEW | FmtSpan::CLOSE))
         .init();
     info!("Initialized: Tracing");
+    if dotenv_loaded.is_some() {
+        info!("Loaded file: {}", dotenv_loaded.unwrap().display());
+    }
+    if enabled!(Level::TRACE) {
+        for var in dotenvy::vars() {
+            trace!("Loaded envar: {}={}", var.0, var.1);
+        }
+    }
     let user_store = HashmapUserStore::default();
     info!("Initialized: User store");
     let app_state = AppState::new(Arc::new(RwLock::new(user_store)));
